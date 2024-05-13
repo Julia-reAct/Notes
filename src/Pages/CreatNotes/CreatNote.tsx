d5838f58e997a58b07ba6edbbcc8790d030be92d
Yulia <yuliakiss369@gmail.com>
2024-05-13 18:38:51 +0300

    Add Form for Creat Note

--- src/Pages/CreatNotes/CreatNote.tsx
+++ src/Pages/CreatNotes/CreatNote.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Formik, Field, Form, FormikHelpers } from 'formik';
+
+interface Values {
+    title: string;
+    text: string;
+}
 
 function CreatNote() {
     return (
-        <div></div>
+        <div>
+            <h1>Signup</h1>
+            <Formik
+                initialValues={{
+                    title: "",
+                    text: "",
+                }}
+                onSubmit={(
+                    values: Values,
+                    { setSubmitting }: FormikHelpers<Values>
+                ) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 500);
+                }}
+            >
+                <Form>
+                    <label htmlFor="title">Title</label>
+                    <Field id="title" name="title" placeholder="Title" />
+
+                    <label htmlFor="text">Text</label>
+                    <Field id="text" name="text" placeholder="Text" />
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
+        </div>
 
 );
 }
