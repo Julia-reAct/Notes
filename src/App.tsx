a4bbda2bb1144d25565898cdd181bbda22f2bbb1
Yulia <yuliakiss369@gmail.com>
2024-05-13 18:19:54 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+import MainNote from "./Pages/MainPages/MainPage";
+import CreatNote from "./Pages/CreatNotes/CreatNote";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <HashRouter>
+              <Routes>
+                  <Route path="/" element={<MainNote/>}></Route>
+                  <Route path="/creatNote" element={<CreatNote/>}></Route>
+              </Routes>
+          </HashRouter>
       </header>
     </div>
   );
--- src/Pages/MainPages/MainPage.tsx
+++ src/Pages/MainPages/MainPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function CreatNote() {
+function MainNote() {
     return (
         <div></div>
 
     );
 }
 
-export default CreatNote;
+export default MainNote;
